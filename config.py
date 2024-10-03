470b5038b3eb9536eeafb9f84d896b24d9882723
timf34 <66926418+timf34@users.noreply.github.com>
2024-10-03 03:49:36 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ OPENAI_KEY = "add-your-openai-api-key-here"
 INDIVIDUAL_RECIPIENT_PHONE_NUMBER = "+353871234567"
 WHATSAPP_GROUPCHAT_GROUP_ID = "abcdefghijklmnopqrstuvwxyz"  # Group ID is found from the invite link of the group chat
 
-# General settings
 MEETING_TIME = "9pm"  # Default meeting time
 MODEL = "gpt-4o"
