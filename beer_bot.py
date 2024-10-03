cf0538d0e672afaf232b41b4104112a9bf4a92dc
timf34 <farrelti@tcd.ie>
2024-10-03 02:12:54 +0100

    updates

--- beer_bot.py
+++ beer_bot.py
@@ -4,6 +4,9 @@ from secrets import OPENAI_KEY
 
 client = openai.Client(api_key=OPENAI_KEY)
 
+specific_contact = "+353834551238"
+specific_group_id = "JKjj0IEW4pLK87sBj53jBY"  # Group ID is found from the invite link of the group chat
+
 
 def generate_poem():
     prompt = "Compose an original Irish poem, encouraging one's friends to get a pint with them at 9pm this evening. Specifically we'll meet at Chaplines Pub at 9pm. Keep it short and concise, and following the typical old trad music/ traditional Irish pub structure of written in any number of quatrains. alliterated, 2 word alliteration in each line. written with aicill rhyme, the end word of L3 internally rhymes with L4. But keep it to 4 lines long, finishing with a poetic line to meet at Chaplins at 9pm, you must mention 9pm, and follow old Irish style and references"
@@ -19,9 +22,7 @@ def generate_poem():
     return poem
 
 poem = generate_poem()
-
-# Append "Yours sincerely, \n BeerBot" to the poem
 poem += "\nWith love, \nBeerBot"
 
 print(poem)
-# pywhatkit.sendwhatmsg_to_group_instantly("", poem)
+pywhatkit.sendwhatmsg_to_group_instantly(specific_contact, poem)
