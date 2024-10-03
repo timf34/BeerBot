e4de4612479abf45a3d389f8106edd21091f5de1
timf34 <farrelti@tcd.ie>
2024-10-03 04:18:32 +0100

    added candidate pubs to config.py

--- config.py
+++ config.py
@@ -4,4 +4,11 @@ INDIVIDUAL_RECIPIENT_PHONE_NUMBER = "+353871234567"
 WHATSAPP_GROUPCHAT_GROUP_ID = "abcdefghijklmnopqrstuvwxyz"  # Group ID is found from the invite link of the group chat
 
 MEETING_TIME = "9pm"  # Default meeting time
+CANDIDATE_PUBS = [
+    "Chaplins Pub",
+    "Doyles",
+    "The Pav",
+    "O'Regan's",
+]
+
 MODEL = "gpt-4o"
